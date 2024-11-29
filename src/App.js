39eef7d2e773ddce2e86a775dfa6db9732cc919a
Yuan Zhang <mitchellcheung1@yahoo.com>
2024-11-29 20:34:55 +0000

    add first function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/upload_image`)).json();
+      setData(text);
+    })();
+  });
+
+  return <h1>{data}</h1>;
 }
 
 export default App;
